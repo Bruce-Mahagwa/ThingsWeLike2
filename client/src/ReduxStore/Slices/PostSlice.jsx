ef6e709b8003b0a92735a34f9907765310479e50
Bruce-Mahagwa <96821698+Bruce-Mahagwa@users.noreply.github.com>
2024-02-20 12:26:39 +0300

    Add files via upload

---
+++ client/src/ReduxStore/Slices/PostSlice.jsx
@@ -0,0 +1,53 @@
+// files
+import { createSlice } from '@reduxjs/toolkit';
+import { getPosts, getPost, createPost } from "../Actions/PostActions";
+
+// variables
+const initialState = {
+  posts: { data: [], total: 0, pageNum: 1, pageSize: 3 },
+  post: { data: {} },
+  spaceName: "",
+  error: null,
+  status: "idle"
+}
+
+const PostSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    clearPosts(state) {
+      // empties the posts data array
+      state.posts.data = [];
+    },
+    getPostsFromSocketIo(state, post) {
+      state.posts.data = [post.payload, ...state.posts.data];
+      console.log("slice", state.posts.data);
+    }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getPosts.fulfilled, (state, action) => {
+      // console.log(action.payload)
+      if (action.payload.prefix === "posts") {
+        state.posts.data = action.payload.data_copy.data
+      }
+      else {
+        state.posts.data = [...state.posts.data, ...action.payload.data_copy.data];
+      }
+      state.posts.total = action.payload.data_copy.total;
+      state.posts.pageNum = action.payload.data_copy.pageNum;
+      state.posts.pageSize = action.payload.data_copy.pageSize;
+      state.spaceName = action.payload.data_copy.spaceName;
+    }).addCase(getPosts.rejected, (state, action) => {
+      state.error = action.payload;
+    }).addCase(createPost.rejected, (state, action) => {
+      state.error = action.payload;
+    }).addCase(getPost.fulfilled, (state, action) => {
+      state.post.data = action.payload.data
+    }).addCase(getPost.rejected, (state, action) => {
+      state.error = action.payload;
+    })
+  }
+})
+
+export default PostSlice.reducer;
+export const { clearPosts, getPostsFromSocketIo } = PostSlice.actions; 
