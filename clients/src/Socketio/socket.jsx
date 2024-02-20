297cb8afdd1abde9147de9cddedecd8ca5211fda
Bruce-Mahagwa <96821698+Bruce-Mahagwa@users.noreply.github.com>
2024-02-20 13:24:31 +0300

    Update socket.jsx

--- clients/src/Socketio/socket.jsx
+++ clients/src/Socketio/socket.jsx
@@ -4,6 +4,6 @@ import { io } from "socket.io-client";
 // export const socket = io("https://a62cd8cb-6249-47ab-8ce2-89e6d92956a3-00-2so2id1sckb1k.picard.replit.dev", {
 
 // });
-export const socket = io("http://localhost:4000", {
+export const socket = io("https://things-we-like-api.vercel.app", {
 
 });
