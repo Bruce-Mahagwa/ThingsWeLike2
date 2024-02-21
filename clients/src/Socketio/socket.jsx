216ccecf663eaacb13de1421433182c554ed6612
Bruce-Mahagwa <96821698+Bruce-Mahagwa@users.noreply.github.com>
2024-02-21 11:26:29 +0300

    Update socket.jsx

--- clients/src/Socketio/socket.jsx
+++ clients/src/Socketio/socket.jsx
@@ -4,7 +4,7 @@ import { io } from "socket.io-client";
 // export const socket = io("https://a62cd8cb-6249-47ab-8ce2-89e6d92956a3-00-2so2id1sckb1k.picard.replit.dev", {
 
 // });
-export const socket = io("https://things-we-like-api.vercel.app", {
+export const socket = io("https://thingswelikeapi.onrender.com", {
   transports: [ "websocket" ],
   withCredentials: true
 });
