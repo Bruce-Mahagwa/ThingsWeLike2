05140cf43548b10bfa23565f9c267d92df51e40e
Bruce-Mahagwa <96821698+Bruce-Mahagwa@users.noreply.github.com>
2024-02-22 09:49:23 +0300

    Update socket.jsx

--- clients/src/Socketio/socket.jsx
+++ clients/src/Socketio/socket.jsx
@@ -1,11 +1,14 @@
 // dependencies
-import { io } from "socket.io-client";
+// import { io } from "socket.io-client";
 // variables
 // export const socket = io("https://a62cd8cb-6249-47ab-8ce2-89e6d92956a3-00-2so2id1sckb1k.picard.replit.dev", {
 
 // });
-export const socket = io("https://thingswelikeapi.onrender.com", {
-  // we connect to this url for our websocket 
-  transports: [ "websocket" ],
-  withCredentials: true
-});
+// export const socket = io("https://thingswelikeapi.onrender.com", {
+//   // we connect to this url for our websocket 
+//   transports: [ "websocket" ],
+//   withCredentials: true
+// });
+export const socket = () => {
+  return;
+}
