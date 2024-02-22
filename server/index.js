6d60de04f6c5e495bb8588a2bfc5438c3f3fdbab
Bruce-Mahagwa <96821698+Bruce-Mahagwa@users.noreply.github.com>
2024-02-22 09:09:15 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,81 +38,82 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(apiRoutes);
 // socket io
-const server = require("http").createServer(app);
-const io = require("socket.io")(server, { 
-  cors: { 
-    origin: ["https://things-we-like-client.vercel.app", "https://things-we-like-client.vercel.app/", "http://things-we-like-client.vercel.app", "http://things-we-like-client.vercel.app/"], 
-    credentials: true
-  }
-});
+// const server = require("http").createServer(app);
+// const io = require("socket.io")(server, { 
+//   cors: { 
+//     origin: ["https://things-we-like-client.vercel.app", "https://things-we-like-client.vercel.app/", "http://things-we-like-client.vercel.app", "http://things-we-like-client.vercel.app/"], 
+//     credentials: true
+//   }
+// });
 
 // connect
 connectDB();
 // listen
-server.listen(process.env.PORT);
+// server.listen(process.env.PORT);
 
 // Socket.IO connections
-io.on("connection", (socket) => {
-  console.log(`Socket ${socket.id} connected`);
-  const count = io.engine.clientsCount;
-  console.log("number of users is", count);
-  socket.on("writeComment", async (data) => {
-    try {
-      await connectDB();
-      const postId = data.postId;
-      const memberId = data.owner;
-      const userName = data.userName;
-      const postedAt = new Date();
-      const comment = data.comment;
-      const newComment = await CommentsModel.create({
-        postId,
-        comment: comment,
-        postedAt: postedAt,
-        owner: memberId,
-        userName: userName,
-      });
-      io.emit("comment", newComment);
+// io.on("connection", (socket) => {
+//   console.log(`Socket ${socket.id} connected`);
+//   const count = io.engine.clientsCount;
+//   console.log("number of users is", count);
+//   socket.on("writeComment", async (data) => {
+//     try {
+//       await connectDB();
+//       const postId = data.postId;
+//       const memberId = data.owner;
+//       const userName = data.userName;
+//       const postedAt = new Date();
+//       const comment = data.comment;
+//       const newComment = await CommentsModel.create({
+//         postId,
+//         comment: comment,
+//         postedAt: postedAt,
+//         owner: memberId,
+//         userName: userName,
+//       });
+//       io.emit("comment", newComment);
       
-      const post = await PostModel.findById(postId);
-      post.comments.push({ owner: memberId, postId: newComment._id });
-      await post.save();
-    } catch (e) {
-      io.emit("commentingError", e.message)
-    }
-  });
-  socket.on("writePostInPosts", async (data) => {
-    const count = io.engine.clientsCount;
-    console.log("number of users is", count);
-    try {
-      await connectDB();
-      const spaceId = data.spaceId;
-      const userId = data.userId;
-      const userName = data.userName;
+//       const post = await PostModel.findById(postId);
+//       post.comments.push({ owner: memberId, postId: newComment._id });
+//       await post.save();
+//     } catch (e) {
+//       io.emit("commentingError", e.message)
+//     }
+//   });
+//   socket.on("writePostInPosts", async (data) => {
+//     const count = io.engine.clientsCount;
+//     console.log("number of users is", count);
+//     try {
+//       await connectDB();
+//       const spaceId = data.spaceId;
+//       const userId = data.userId;
+//       const userName = data.userName;
 
-      const ifisMember = await SpaceModel.findById(spaceId).elemMatch(
-        "members",
-        {
-          memberId: userId,
-          userName: userName,
-        },
-      );
-      const description = data.description;
-      const postedAt = new Date();
-      const post = await PostModel.create({
-        description,
-        postedAt,
-        owner: { memberId: userId, userName },
-      });
-      io.emit("postInPosts", post);
-      const save_post = await SpaceModel.findById(spaceId);
-      save_post.posts.push({ owner: userId, postId: post._id });
-      await save_post.save();
-    } catch (e) {
-      io.emit("postingError", e.message);
-    }
-  });
+//       const ifisMember = await SpaceModel.findById(spaceId).elemMatch(
+//         "members",
+//         {
+//           memberId: userId,
+//           userName: userName,
+//         },
+//       );
+//       const description = data.description;
+//       const postedAt = new Date();
+//       const post = await PostModel.create({
+//         description,
+//         postedAt,
+//         owner: { memberId: userId, userName },
+//       });
+//       io.emit("postInPosts", post);
+//       const save_post = await SpaceModel.findById(spaceId);
+//       save_post.posts.push({ owner: userId, postId: post._id });
+//       await save_post.save();
+//     } catch (e) {
+//       io.emit("postingError", e.message);
+//     }
+//   });
 
-  socket.on("disconnect", () => {
-    console.log(`Socket ${socket.id} disconnected`);
-  });
-});
+//   socket.on("disconnect", () => {
+//     console.log(`Socket ${socket.id} disconnected`);
+//   });
+// });
+app.listen(PORT)
