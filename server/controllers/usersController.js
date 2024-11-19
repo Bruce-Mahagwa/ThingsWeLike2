09172047dca7b44add6037ef482efb2c77c11ebe
Bruce-Mahagwa <jacobbruce880@gmail.com>
2024-11-19 14:17:58 +0300

    Change error code message to 503 for register user controller

--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -25,11 +25,11 @@ const registerUser = async (req, res) => {
     const email = req.body?.email;
     const password = req.body?.password;
     if (!(userName && email && password)) {
-      return res.status(400).json({ error: "Please provide all inputs" });
+      return res.status(503).json({ error: "Please provide all inputs" });
     }
     const user = await UserModel.findOne({ email: email });
     if (user) {
-      return res.status(400).json({ error: "User already exists" });
+      return res.status(503).json({ error: "User already exists" });
     }
     const checkSameUsername = await UserModel.findOne({ userName: userName });
     if (checkSameUsername) {
